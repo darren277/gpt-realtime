b2a117c45a28f4fd2b485ddcfe964c9953944c32
darren277 <darren277@yahoo.com>
2024-12-26 08:15:55 -0800

    Alternative WAV converter.

--- js/utils.js
+++ js/utils.js
@@ -1,4 +1,4 @@
-function encodeWAV(pcmData, sampleRate = 24000, numChannels = 1) {
+function encodeWAV_old(pcmData, sampleRate = 24000, numChannels = 1) {
     const wavHeader = new ArrayBuffer(44);
     const view = new DataView(wavHeader);
   
@@ -27,4 +27,41 @@ function encodeWAV(pcmData, sampleRate = 24000, numChannels = 1) {
     return new Uint8Array([...new Uint8Array(wavHeader), ...pcmData]);
 }
 
+function encodeWAV(pcmData, sampleRate = 24000, numChannels = 1) {
+    const buffer = new ArrayBuffer(44 + pcmData.length);
+    const view = new DataView(buffer);
+  
+    // RIFF chunk descriptor
+    writeString(view, 0, 'RIFF');
+    view.setUint32(4, 36 + pcmData.length, true); // File size - 8
+    writeString(view, 8, 'WAVE');
+  
+    // fmt sub-chunk
+    writeString(view, 12, 'fmt ');
+    view.setUint32(16, 16, true); // Subchunk1 size (16 for PCM)
+    view.setUint16(20, 1, true); // Audio format (1 for PCM)
+    view.setUint16(22, numChannels, true); // Number of channels
+    view.setUint32(24, sampleRate, true); // Sample rate
+    view.setUint32(28, sampleRate * numChannels * 2, true); // Byte rate
+    view.setUint16(32, numChannels * 2, true); // Block align
+    view.setUint16(34, 16, true); // Bits per sample
+  
+    // data sub-chunk
+    writeString(view, 36, 'data');
+    view.setUint32(40, pcmData.length, true); // Data size
+  
+    // Write PCM data
+    for (let i = 0; i < pcmData.length; i++) {
+      view.setInt8(44 + i, pcmData[i]);
+    }
+  
+    return new Uint8Array(buffer);
+  }
+  
+  function writeString(view, offset, string) {
+    for (let i = 0; i < string.length; i++) {
+      view.setUint8(offset + i, string.charCodeAt(i));
+    }
+  }
+  
 module.exports = { encodeWAV };
