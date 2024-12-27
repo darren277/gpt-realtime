ee4bce698082ce95644da4c3c4f1101121a78378
darren277 <darren277@yahoo.com>
2024-12-27 13:24:12 -0800

    A sample function handler.

---
+++ js/functionHandlers.js
@@ -0,0 +1,36 @@
+const functions = [];
+
+functions.push({
+  schema: {
+    name: "get_weather_from_coords",
+    type: "function",
+    description: "Get the current weather",
+    parameters: {
+      type: "object",
+      properties: {
+        latitude: {
+          type: "number",
+        },
+        longitude: {
+          type: "number",
+        },
+      },
+      required: ["latitude", "longitude"],
+    },
+  },
+  handler: async (args) => {
+    console.log("get_weather_from_coords", args);
+
+    if (!args.latitude || !args.longitude) {
+      throw new Error("Missing required parameters");
+    }
+    const response = await fetch(
+      `https://api.open-meteo.com/v1/forecast?latitude=${args.latitude}&longitude=${args.longitude}&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m`
+    );
+    const data = await response.json();
+    const currentTemp = data.current?.temperature_2m;
+    return JSON.stringify({ temp: currentTemp });
+  },
+});
+
+module.exports = { functions };
