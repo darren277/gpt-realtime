d7b0abff8b06e9a12a4828cadafa8a1c74e6cd45
darren277 <darren277@yahoo.com>
2025-02-11 02:13:18 -0800

    Created a WebSocketClient.

---
+++ py/static/js/ws-client.js
@@ -0,0 +1,48 @@
+export default class WebSocketClient {
+    constructor(url) {
+        this.url = url;
+        this.socket = null;
+        this.connect();
+    }
+
+    connect() {
+        this.socket = new WebSocket(this.url);
+
+        // Bind event handlers
+        this.socket.onopen = (event) => this.handleOpen(event);
+        this.socket.onmessage = (event) => this.handleMessage(event);
+        this.socket.onerror = (error) => this.handleError(error);
+        this.socket.onclose = (event) => this.handleClose(event);
+    }
+
+    // Override these methods as needed in your application
+    handleOpen(event) {
+        console.log('WebSocket connection opened:', event);
+        if (this.onOpen) this.onOpen(event);
+    }
+
+    handleMessage(event) {
+        console.log('WebSocket message received:', event.data);
+        if (this.onMessage) this.onMessage(event);
+    }
+
+    handleError(error) {
+        console.error('WebSocket error:', error);
+        if (this.onError) this.onError(error);
+    }
+
+    handleClose(event) {
+        console.log('WebSocket connection closed:', event);
+        if (this.onClose) this.onClose(event);
+        // Optionally, you might want to reconnect here:
+        // this.connect();
+    }
+
+    send(data) {
+        if (this.socket && this.socket.readyState === WebSocket.OPEN) {
+            this.socket.send(data);
+        } else {
+            console.error('WebSocket is not open. Ready state:', this.socket.readyState);
+        }
+    }
+}
